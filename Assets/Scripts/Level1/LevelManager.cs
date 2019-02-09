545acee62941d150c814a04aeb8062d65d58f809
SKayJaySK <SKayJaySK@users.noreply.github.com>
2019-02-09 22:03:36 +0530

    Level 1 Prototype v0.3

--- Assets/Scripts/Level1/LevelManager.cs
+++ Assets/Scripts/Level1/LevelManager.cs
@@ -1,46 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-    public List<GameObject> TBI;
-    public List<GameObject> TBAA;
-    public GameObject Black;
-
-    List<GameObject> TBA;
-
     public Material Ill;
 
+    public Image text;
+    public Button exit;
+
     public static bool ill = false;
+    bool quitting = false, quitting2 = false;
 
     private void Start()
     {
-        TBA = new List<GameObject>();
-
-        /*for (int i = 0; i < Black.transform.childCount; i++)
-        {
-            TBA.Add(Black.transform.GetChild(i).gameObject);
-            Black.transform.GetChild(i).gameObject.SetActive(false);
-        }*/
+        text.gameObject.SetActive(false);
+        exit.gameObject.SetActive(false);
+        Ill.color = new Color(0, 0, 0, 1);
+        Ill.SetColor("_EmissionColor", Color.black);
     }
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.R))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         if (ill)
         {
-            for (int i = 0; i < TBI.Count; i++)
-            {
-                TBI[i].GetComponent<Renderer>().material = Ill;
-            }
-            /*for (int i = 0; i < TBA.Count; i++)
-            {
-                TBA[i].SetActive(true);
-            }
-            for (int i = 0; i < TBAA.Count; i++)
-            {
-                TBAA[i].SetActive(true);
-            }*/
+            Ill.color = new Color(0, 1, 0.6f, 1);
+            Ill.SetColor("_EmissionColor", new Color(0, 1, 0.8f, 1));
         }
+        if (quitting && quitting2)
+            Application.Quit();
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (!quitting)
+        {
+            Application.CancelQuit();
+            text.gameObject.SetActive(true);
+            exit.gameObject.SetActive(true);
+            quitting = true;
+        }
+    }
+
+    public void ExitButton()
+    {
+        quitting2 = true;
     }
 }
---
+++ Assets/Scripts/PrefabScripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed;
+    public float dashSpeed, dashTime;
+    [SerializeField] float currentDashTime;
+    bool isDashing = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        currentDashTime = dashTime;
+    }
+    
+    void Update()
+    {
+        float moveX= Input.GetAxisRaw("Horizontal");
+        float moveZ = Input.GetAxisRaw("Vertical");
+        if (!isDashing)
+        {
+            if (moveX == 0)
+            {
+                rb.velocity = new Vector3(0, rb.velocity.y, rb.velocity.z);
+            }
+            else
+            {
+                rb.velocity = new Vector3(moveX * speed, rb.velocity.y, rb.velocity.z);
+            }
+            if (moveZ == 0)
+            {
+                rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, 0);
+            }
+            else
+            {
+                rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, moveZ * speed);
+            }
+
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                rb.velocity = new Vector3(moveX * dashSpeed, rb.velocity.y, moveZ * dashSpeed);
+                isDashing = true;
+            }
+
+            if (rb.velocity.y > 1f)
+                rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+            if (rb.velocity.y < 0)
+                rb.velocity = new Vector3(rb.velocity.x, Mathf.Abs(1.3f * rb.velocity.y) * -1, rb.velocity.z);
+        }
+        else
+        {
+            currentDashTime -= Time.deltaTime;
+            if (currentDashTime <= 0)
+            {
+                rb.velocity = new Vector3(0, rb.velocity.y, 0);
+                isDashing = false;
+                currentDashTime = dashTime;
+            }
+        }
+    }
+}
