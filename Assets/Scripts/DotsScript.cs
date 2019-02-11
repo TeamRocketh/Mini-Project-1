7533a3c7ac290de02d0935a351a9aafc844fea9c
SKayJaySK <SKayJaySK@users.noreply.github.com>
2019-02-12 00:57:23 +0530

    Almost final

---
+++ Assets/Scripts/DotsScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DotsScript : MonoBehaviour
+{
+    public List<Sprite> sprites = new List<Sprite>();
+    public Image dots;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        dots = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dots.sprite = sprites[FindObjectOfType<PlayerController>().currentDashCharge];
+    }
+}
